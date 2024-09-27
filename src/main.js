85616bb2c25a0f7027dc1259bf51d6ab4bb8c809
wuchuan123 <609833577@qq.com>
2024-09-27 17:56:04 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/styles/index.scss';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
+Vue.config.productionTip = false;
 new Vue({
+  el: '#app',
   router,
   store,
   render: (h) => h(App),
